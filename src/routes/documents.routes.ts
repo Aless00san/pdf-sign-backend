bd8d6b6a0d1df05e324ae05848aabbd3b5bedd45
Aless00san <mumaitronzx@gmail.com>
2025-09-24 17:03:36 +0200

    Get all documents endpoint

--- src/routes/documents.routes.ts
+++ src/routes/documents.routes.ts
@@ -1,31 +1,31 @@
-import { Router } from 'express';
-import { db } from '../db';
-import { documents } from '../schema';
-import { randomUUID } from 'crypto';
-import { upload } from '../multer';
-import { eq } from 'drizzle-orm';
-import fs from 'fs';
-import path from 'path';
-import QRCode from 'qrcode';
-import { auth } from '../middleware/auth';
+import { Router } from "express";
+import { db } from "../db";
+import { documents } from "../schema";
+import { randomUUID } from "crypto";
+import { upload } from "../multer";
+import { eq } from "drizzle-orm";
+import fs from "fs";
+import path from "path";
+import QRCode from "qrcode";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
 // This route is used to upload a new document to the server
 // It generates a random ID for the document and creates an entry in the database
 router.post(
-  '/documents',
+  "/documents",
   auth,
   (req, res, next) => {
     (req as any).docId = randomUUID();
     next();
   },
-  upload.single('pdf'),
+  upload.single("pdf"),
   (req, res) => {
     console.log(req.file);
 
     if (!req.file)
-      return res.status(400).json({ error: 'Error uploading file' });
+      return res.status(400).json({ error: "Error uploading file" });
 
     const docId = (req as any).docId;
     const userId = (req as any).userId;
@@ -34,22 +34,21 @@ router.post(
       .values({
         id: docId,
         name: req.file.originalname,
-        status: 'pending',
+        status: "pending",
         userId: userId,
       })
       .run();
 
     res
       .status(201)
-      .json({ docId, name: req.file.originalname, status: 'pending' });
+      .json({ docId, name: req.file.originalname, status: "pending" });
   }
 );
 
 // This route generates a QR code for signing the document on the client-side
-router.get('/documents/:id/qr', async (req, res) => {
+router.get("/documents/:id/qr", async (req, res) => {
   const docId = req.params.id;
 
-  // Verificar que el documento existe
   const doc = await db
     .select()
     .from(documents)
@@ -57,7 +56,7 @@ router.get('/documents/:id/qr', async (req, res) => {
     .get();
 
   if (!doc) {
-    return res.status(404).json({ error: 'Document not found' });
+    return res.status(404).json({ error: "Document not found" });
   }
 
   // FRONTEND URL
@@ -71,12 +70,12 @@ router.get('/documents/:id/qr', async (req, res) => {
       qr: qrDataUrl,
     });
   } catch (err) {
-    console.error('Error generando QR:', err);
-    res.status(500).json({ error: 'Error generating QR code' });
+    console.error("Error generando QR:", err);
+    res.status(500).json({ error: "Error generating QR code" });
   }
 });
 
-router.delete('/documents/:id', auth, async (req, res) => {
+router.delete("/documents/:id", auth, async (req, res) => {
   const docId = req.params.id;
 
   const doc = await db
@@ -86,11 +85,11 @@ router.delete('/documents/:id', auth, async (req, res) => {
     .get();
 
   if (!doc) {
-    return res.status(404).json({ error: 'Document not found' });
+    return res.status(404).json({ error: "Document not found" });
   }
 
   const filePath = path.join(
-    'storage/docs',
+    "storage/docs",
     `${docId}${path.extname(doc.name)}`
   );
 
@@ -99,10 +98,10 @@ router.delete('/documents/:id', auth, async (req, res) => {
   // Delete entry from database
   db.delete(documents).where(eq(documents.id, docId)).run();
 
-  return res.status(200).json({ message: 'Document successfully deleted' });
+  return res.status(200).json({ message: "Document successfully deleted" });
 });
 
-router.get('/documents/:id/download', auth, async (req, res) => {
+router.get("/documents/:id/download", auth, async (req, res) => {
   const userId = (req as any).userId;
   const { id } = req.params;
 
@@ -112,27 +111,39 @@ router.get('/documents/:id/download', auth, async (req, res) => {
     .where(eq(documents.id, id))
     .get();
 
-  if (!doc) return res.status(404).json({ error: 'Document not found' });
+  if (!doc) return res.status(404).json({ error: "Document not found" });
 
   if (doc.userId !== userId)
     return res.status(403).json({
-      error: 'Forbidden: You are not authorized to access this document',
+      error: "Forbidden: You are not authorized to access this document",
     });
 
-  const filePath = path.join('storage/docs', `${id}${path.extname(doc.name)}`);
+  const filePath = path.join("storage/docs", `${id}${path.extname(doc.name)}`);
 
-  res.download(filePath, doc.name, err => {
+  res.download(filePath, doc.name, (err) => {
     if (err) {
       res
         .status(500)
-        .json({ error: 'Something went wrong while downloading the document' });
+        .json({ error: "Something went wrong while downloading the document" });
     }
   });
 
-  if (doc.status === 'signed') {
+  if (doc.status === "signed") {
     await fs.unlinkSync(filePath);
     db.delete(documents).where(eq(documents.id, id)).run();
   }
+});
+
+//Get all documents for a user
+router.get("/documents", auth, async (req, res) => {
+  const userId = (req as any).userId;
+  const docs = await db
+    .select()
+    .from(documents)
+    .where(eq(documents.userId, userId))
+    .all();
+  if (!docs) return res.status(404).json({ error: "No documents found" });
+  return res.status(200).json(docs);
 });
 
 export default router;
